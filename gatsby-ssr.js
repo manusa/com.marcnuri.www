637d30019fbf877c82dbd0fcbd082c65a3ce59a5
Marc Nuri <marc@marcnuri.com>
2019-07-15 18:04:14 +0200

    #19: Added Google Search Console verification meta tag

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,7 @@ const React = require('react');
 const {IntlProvider} = require('react-intl');
 const messages = require('./src/i18n/messages');
 const kapo = require('./src/pasko/kapo');
+const {SearchConsoleVerification} = require('./src/components/google/search-console');
 const fostoKorpo = require('./src/pasko/fosto-korpo');
 
 exports.wrapPageElement = ({element, props}) => {
@@ -16,6 +17,6 @@ exports.onPreRenderHTML = ({
   getHeadComponents, replaceHeadComponents,
   getPostBodyComponents, replacePostBodyComponents
 }) => {
-  replaceHeadComponents([kapo, ...getHeadComponents()]);
+  replaceHeadComponents([kapo, <SearchConsoleVerification />, ...getHeadComponents()]);
   replacePostBodyComponents([fostoKorpo, ...getPostBodyComponents()]);
 };
---
+++ src/components/google/search-console.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SearchConsoleVerification = () => (
+  <meta name="google-site-verification" content="V79V8YEhPluHC2nzYUaggmOlI-Li7C7yWspKSHZjZ0A" />
+);
