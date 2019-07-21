418fee3e211ec4b804ec5e6b4018f41eec49e1dc
Marc Nuri <marc@marcnuri.com>
2019-07-21 17:56:47 +0200

    #22: MVP with improved design
    - WIP to debug real device info

--- src/scrum-poker/components/poker-card-front.jsx
+++ src/scrum-poker/components/poker-card-front.jsx
@@ -4,13 +4,24 @@ import PokerCard from './poker-card';
 
 const PokerCardFront = ({symbol, onClick}) => (
   <PokerCard className="poker-card--front" onClick={onClick}>
+    <div className="poker-card__background">
+      <div className="poker-card__inner-background" />
+    </div>
     <div className="poker-card__column-container">
       <div className="poker-card__row-top">
-        <div className="poker-card__symbol-left">
-          {symbol}
+        <div className="poker-card__symbol poker-card__symbol--left">
+          <svg viewBox="0 0 30 18">
+            <text y="9" dominantBaseline="middle">
+              {symbol}
+            </text>
+          </svg>
         </div>
-        <div className="poker-card__symbol-right">
-          {symbol}
+        <div className="poker-card__symbol poker-card__symbol--right">
+          <svg viewBox="0 0 30 18">
+            <text x="100%" y="9" dominantBaseline="middle" textAnchor="end">
+              {symbol}
+            </text>
+          </svg>
         </div>
       </div>
       <div className="poker-card__row-middle">
@@ -21,11 +32,19 @@ const PokerCardFront = ({symbol, onClick}) => (
         </svg>
       </div>
       <div className="poker-card__row-bottom">
-        <div className="poker-card__symbol-left">
-          {symbol}
+        <div className="poker-card__symbol poker-card__symbol--left">
+          <svg viewBox="0 0 30 18">
+            <text x="100%" y="9" dominantBaseline="middle" textAnchor="end">
+              {symbol}
+            </text>
+          </svg>
         </div>
-        <div className="poker-card__symbol-right">
-          {symbol}
+        <div className="poker-card__symbol poker-card__symbol--right">
+          <svg viewBox="0 0 30 18">
+            <text y="9" dominantBaseline="middle">
+              {symbol}
+            </text>
+          </svg>
         </div>
       </div>
     </div>
