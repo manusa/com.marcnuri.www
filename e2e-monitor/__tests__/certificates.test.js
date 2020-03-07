96cc0430faf4911d84388c14ac8f47528dd0e9cb
Marc Nuri <marc@marcnuri.com>
2020-03-07 07:13:52 +0100

    feature: Monitor mail server certificates

--- e2e-monitor/__tests__/certificates.test.js
+++ e2e-monitor/__tests__/certificates.test.js
@@ -1,7 +1,7 @@
 const tls = require('tls');
 
-const checkHttpsCertificate = (hostname, done) => {
-  const socket = tls.connect(443, hostname, () => {
+const checkCertificate = port => (hostname, done) => {
+  const socket = tls.connect(port, hostname, () => {
     const certificate = socket.getPeerCertificate();
     expect(certificate.subject.CN).toMatch(/.*marcnuri.com$/);
     const daysToExpiry = (new Date(certificate.valid_to) - new Date()) / (24 * 60 * 60 * 1000);
@@ -9,6 +9,10 @@ const checkHttpsCertificate = (hostname, done) => {
     done();
   });
 };
+
+const checkHttpsCertificate = checkCertificate(443);
+const checkIMAPSCertificate = checkCertificate(993);
+const checkSMTSCertificate = checkCertificate(465);
 
 describe('Check SSL certificates', () => {
   beforeEach(() => {
@@ -18,6 +22,8 @@ describe('Check SSL certificates', () => {
   test('www.marcnuri.com', done => checkHttpsCertificate('www.marcnuri.com', done));
   test('blog.marcnuri.com', done => checkHttpsCertificate('blog.marcnuri.com', done));
   test('isotope.marcnuri.com', done => checkHttpsCertificate('isotope.marcnuri.com', done));
-  test('mail.marcnuri.com', done => checkHttpsCertificate('mail.marcnuri.com', done));
+  test('mail.marcnuri.com:443', done => checkHttpsCertificate('mail.marcnuri.com', done));
+  test('mail.marcnuri.com:993', done => checkIMAPSCertificate('mail.marcnuri.com', done));
+  test('mail.marcnuri.com:465', done => checkSMTSCertificate('mail.marcnuri.com', done));
 });
 
