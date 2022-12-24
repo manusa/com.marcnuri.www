c275ac7b975c8b4e02970acc48b9c1a18ee0a686
Marc Nuri <marc@marcnuri.com>
2022-12-24 09:01:02 +0100

    ripari: afiŝkorpo uzas klavojn por pluraj elementoj

--- src/pasko/fosto-korpo.js
+++ src/pasko/fosto-korpo.js
@@ -12,8 +12,8 @@ const consoleBanner = `%c
 
 export const FostoKorpo = () => (
   <>
-    <style dangerouslySetInnerHTML={{__html: `\n/**\n${fostoKorpoFonto}\n**/\n`}} />
-    <script dangerouslySetInnerHTML={{__html: `
+    <style key='fonto' dangerouslySetInnerHTML={{__html: `\n/**\n${fostoKorpoFonto}\n**/\n`}} />
+    <script key='stipo' dangerouslySetInnerHTML={{__html: `
       if (console.log) console.log('${consoleBanner}',
         'font-family:monospace;font-size:12px;color:#2f2739;');`
     }} />
